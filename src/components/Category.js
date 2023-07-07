a85ddca7edbf4e0d6aaff7cd6275870cb280dac9
davep80 <davidpaq1@gmail.com>
2023-07-07 13:22:55 -0400

    made create categories enable component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,31 +1,79 @@
 import axios from 'axios'
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Dialog, Transition, Switch } from '@headlessui/react'
 
 function Category() {
     //Income ++
     //Expenses --
     //Saving +-
-    const [category, setCategory] = useState(null)
+    const [category, setCategory] = useState([])
     const [entry, setEntry] = useState([])
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [modal, setModal] = useState(false);
+    const [isOpen, setIsOpen] = useState(true)
+    const [enabled, setEnabled] = useState(false)
     const navigate = useNavigate()
     async function getCategories() {
         await axios.get('http://localhost:9000/category').then(res => { setCategory(res.data) }).catch(e => console.log(e))
     }
     async function getEntries() {
-        await axios.get('http://localhost:9000/transactions').then(res => { console.log(res.data); setEntry(res.data) }).catch(e => console.log(e))
+        await axios.get('http://localhost:9000/transactions').then(res => { setEntry(res.data) }).catch(e => console.log(e))
+    }
+    async function getEnable() {
+        await axios.get('http://localhost:9000/enable').then(res => { setEnabled(res.data[0]) }).catch(e => console.log(e))
     }
     useEffect(() => {
-        getCategories()
         getEntries()
+        getEnable()
+        getCategories()
     }, [])
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let forminfo = Array.from([input1, input2, input3])
+        if (forminfo.every(item => item === '')) return
+        if (forminfo.some(item => category.includes(item))) {
+            alert('duplicate category')
+            return
+        }
+        let newcategories = forminfo.filter(item => item !== '')
+        if (newcategories.length) {
+            await axios.post(`http://localhost:9000/category`, { category: newcategories }).then(res => {
+                setModal(true)
+            }).catch(e => console.log(e))
+            setCategory([...category, ...newcategories])
+        }
+        setInput1('');
+        setInput2('');
+        setInput3('');
+    };
+    const handleSwitch = async () => {
+        setEnabled(!enabled)
+        await axios.post(`http://localhost:9000/enable`, { enabled: !enabled }).then(res => console.log(res)).catch(e => console.log(e))
+    }
     return (
-        <div className='container mx-auto'>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             {
-                entry.length ? <div>
-                    <a onClick={() => navigate('/transactions')}>Your budget table</a>
-                </div> : <div><h2>Get started with a new budget</h2></div>
+                entry.length || enabled ? <div className='py-4'>
+                    <Link
+                        to="/transactions"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                    >Goto your budget table</Link>
+                </div> :
+                    <span className="inline-flex items-center rounded-md bg-pink-50 px-2 py-2 text-m font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">
+                        Get started with a new budget
+                    </span>
             }
             <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
                 <div className="px-6 py-4">
@@ -36,7 +84,24 @@ function Category() {
                         })}
                     </ul>
                     <i>add new categories</i>
-                    <form>
+                    <div>
+                        <h2>{enabled ? 'Your account is active' : 'Start Account'}</h2>
+                        <Switch
+                            checked={enabled}
+                            onChange={handleSwitch}
+                            className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
+                                } relative inline-flex h-6 w-11 items-center rounded-full`}
+                        >
+                            <span className="sr-only">Start Account?</span>
+                            <span
+                                className={`${enabled ? 'translate-x-6' : 'translate-x-1'
+                                    } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                            />
+                        </Switch>
+
+                    </div>
+
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="input1">
                                 Input 1
@@ -46,6 +111,8 @@ function Category() {
                                 id="input1"
                                 type="text"
                                 placeholder="Enter Input 1"
+                                value={input1}
+                                onChange={(e) => setInput1(e.target.value)}
                             />
                         </div>
                         <div className="mb-4">
@@ -57,6 +124,8 @@ function Category() {
                                 id="input2"
                                 type="text"
                                 placeholder="Enter Input 2"
+                                value={input2}
+                                onChange={(e) => setInput2(e.target.value)}
                             />
                         </div>
                         <div className="mb-4">
@@ -68,17 +137,86 @@ function Category() {
                                 id="input3"
                                 type="text"
                                 placeholder="Enter Input 3"
+                                value={input3}
+                                onChange={(e) => setInput3(e.target.value)}
                             />
                         </div>
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button"
+                            type="submit"
                         >
                             Submit
                         </button>
                     </form>
                 </div>
             </div>
+            {
+                modal && <div>
+                    <div className="inset-0 flex py-2 items-center justify-center">
+                        <button
+                            type="button"
+                            onClick={openModal}
+                            className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                        >
+                            Open dialog
+                        </button>
+                    </div>
+
+                    <Transition appear show={isOpen} as={Fragment}>
+                        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="fixed inset-0 bg-black bg-opacity-25" />
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 overflow-y-auto">
+                                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0 scale-95"
+                                        enterTo="opacity-100 scale-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100 scale-100"
+                                        leaveTo="opacity-0 scale-95"
+                                    >
+                                        <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                            <Dialog.Title
+                                                as="h3"
+                                                className="text-lg font-medium leading-6 text-gray-900"
+                                            >
+                                                New Category added
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">
+                                                    {`You can now add transactions under ${category.length} different categories!`}
+                                                </p>
+                                            </div>
+
+                                            <div className="mt-4">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                    onClick={closeModal}
+                                                >
+                                                    Got it, thanks!
+                                                </button>
+                                            </div>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
+                            </div>
+                        </Dialog>
+                    </Transition>
+                </div>
+            }
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,96 @@
 import React from 'react'
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { navLinks } from '../constants';
 import { useState } from 'react';
-import {close, menu, piggy, start} from '../assets'
+import { piggy } from '../assets'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 function Nav() {
     const navigate = useNavigate();
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     return (
-<nav className="bg-purple-500">
-  <div className="container mx-auto flex items-center justify-between py-4">
-    <img src={piggy} alt='pig' className='w-1/4 sm:w-[280px] sm:h-[250px]'/>
-    <img src={start} alt='start' onClick={() => navigate('transactions/category')} className='cursor-pointer'/>
-    <ul className="flex space-x-4 list-none">
-      {navLinks.map((item, i) => {
-        return (
-          <li key={i}>
-            <NavLink
-              to={item.id}
-              className="text-white hover:text-green-500"
-              activeclassname="text-green-500"
-            >
-              {item.title}
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-</nav>
+
+            <header className="absolute inset-x-0 top-0 z-50 bg-purple-500">
+                <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                    <div className="flex lg:flex-1">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                className="h-8 w-auto"
+                                src={piggy}
+                                alt="pig"
+                            />
+                    </div>
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="hidden lg:flex lg:gap-x-12">
+                        {navLinks.map((item) => (
+                            <p key={item.id} onClick={() => navigate(item.id)} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
+                                {item.title}
+                            </p>
+                        ))}
+                    </div>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <p onClick={() => navigate('transactions/category')} className="text-sm font-semibold leading-6 text-gray-900 cursor-pointer">
+                            Get Started <span aria-hidden="true">&rarr;</span>
+                        </p>
+                    </div>
+                </nav>
+                <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <div className="fixed inset-0 z-50" />
+                    <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                                <span className="sr-only">Budget!</span>
+                                <img
+                                    className="h-8 w-auto"
+                                    src={piggy}
+                                    alt="pig"
+                                />
+                          
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {navLinks.map((item, i) => (
+                                        <p
+                                            key={i}
+                                            onClick={() => navigate(item.id)}
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        >
+                                            {item.title}
+                                        </p>
+                                    ))}
+                                </div>
+                                <div className="py-6">
+                                    <p
+                                        onClick={() => navigate('transactions/category')}
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        Get Started
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog.Panel>
+                </Dialog>
+            </header>
+
     )
 }
 
